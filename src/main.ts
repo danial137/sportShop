014077dc6656c8e228091ff05dbc8a6d59c12728
danial137 <danial79fakhrabadi@gmail.com>
2026-02-21 19:26:13 +0330

    feat: update ESLint configuration and modify AppController and main bootstrap error handling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { error } from 'console';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // project description
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  Logger.error('Error');
+});
