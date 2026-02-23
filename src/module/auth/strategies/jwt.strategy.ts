45847ae6820f40257512a08602994db89702082a
danial137 <danial79fakhrabadi@gmail.com>
2026-02-23 20:03:59 +0330

    feat: add @nestjs/passport dependency and implement JWT strategy

--- src/module/auth/strategies/jwt.strategy.ts
+++ src/module/auth/strategies/jwt.strategy.ts
@@ -1,1 +1,6 @@
-// Jwt Strategy for auth requiest 
+// Jwt Strategy for auth requiest
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JWtStrategy extends PassportStrategy(Strategy) {}
