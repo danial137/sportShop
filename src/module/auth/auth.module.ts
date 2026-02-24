832249cba83af561cbb46f183b54b80a9a0b81cd
danial137 <danial79fakhrabadi@gmail.com>
2026-02-24 18:26:00 +0330

    feat: add @nestjs/config dependency and integrate configuration in Auth module

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') ?? 'defaultsecret2026',
+        signOptions: {
+          expiresIn:
+            Number(configService.get<string>('JWT_EXPRIES_IN', '900'))
+        },
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/module/auth/strategies/jwt.strategy.ts
+++ src/module/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,13 @@
 // Jwt Strategy for auth requiest
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class JWtStrategy extends PassportStrategy(Strategy) {}
+export class JWtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private prisma: PrismaService,
+    private configService: ConfigService,
+  ) {}
+}
