74bdaa452edad492ff3cf4dddd5064ac58afd070
danial137 <danial79fakhrabadi@gmail.com>
2026-02-27 16:22:19 +0330

    feat: enhance RefreshTokenStrategy with detailed validation and error handling

--- src/module/auth/strategies/refresh-token-strategy.ts
+++ src/module/auth/strategies/refresh-token-strategy.ts
@@ -5,6 +5,9 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-jwt';
 import { Extractjwt } from 'passport-jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Request } from 'express';
+import * as bycrypt from 'bcrypt';
+import { UnauthorizedException } from '@nestjs/common';
 
 export class RefreshTokenStrategy extends PassportStrategy(
   Strategy,
@@ -23,7 +26,42 @@ export class RefreshTokenStrategy extends PassportStrategy(
   }
 
   // validate refresh token
-    async validate(req: Request, payload: { sub: string; email }) {
-      
+  async validate(req: Request, payload: { sub: string; email: string }) {
+    console.log('RefreshTokenStrategy.validate called');
+    console.log('Payload', { sub: payload.sub, email: payload.email });
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      console.log('No authorization');
+      throw new UnauthorizedException('Refresh token not provided');
+    }
+
+    const refreshToken = authHeader.replace('Baerer', '').trim();
+    if (!refreshToken) {
+      throw new UnauthorizedException(
+        'Refresh token is empty after extraction ',
+      );
+    }
+
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        refreshToken: true,
+      },
+    });
+    if (!user || !user.refreshToken) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+
+    const refreshTokenMatches = await bycrypt.compare(
+      refreshToken,
+      user.refreshToken,
+    );
+    if (!refreshTokenMatches) {
+      throw new UnauthorizedException('Invalid refresh token ');
+    }
   }
 }
